ed3e7925a6bef6d619f342cbd037d4d64a6c8bbf
wuhy15 <wuhy15@lenovo.com>
2022-01-21 18:27:16 +0800

    commit

---
+++ actions/send_exchange_mail.py
@@ -0,0 +1,26 @@
+import os
+from st2common.runners.base_action import Action
+from exchangelib import DELEGATE, Account, Credentials, Message, Mailbox, HTMLBody, FileAttachment, Configuration, NTLM,IMPERSONATION
+
+
+__all__ = [
+    'SendExchangeMailAction'
+]
+
+class SendExchangeMailAction(Action):
+    def run(self, userName,Server,email, emailpw, to_list, subject, message):
+        try:
+            credentials = Credentials(username=userName, password=emailpw)
+            # config = Configuration(server='outlook.office365.com', credentials=credentials)
+            config = Configuration(server=Server, credentials=credentials)
+            account = Account(primary_smtp_address=email, config=config,
+                              autodiscover=False, access_type=DELEGATE)
+            # m=None
+            m = Message(
+                account=account,
+                subject=subject,
+                body=HTMLBody(message),
+                to_recipients=to_list)
+            m.send()
+        except Exception as e:
+            raise
